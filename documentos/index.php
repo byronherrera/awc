6447227f65926cb4a06028b26014c81a225edf69
byron <byronherrera@hotmail.com>
2020-06-12 20:01:51 -0500

    nuevo reporte

--- documentos/index.php
+++ documentos/index.php
@@ -1,6 +1,39 @@
 <?php
+require_once('../server/os.php');
 
-$resultado = obtenerListadoDeArchivos("c:\Program Files (x86)\Zend\Apache24\htdocs\procesos-amc\documentos\archivos", true);
+if (!class_exists('os')) {
+    die('Server os class is missing!');
+} else {
+    $os = new os();
+    if (!$os->session_exists()) {
+        header("Location: login.php");
+    } else {
+        ?>
+        <!DOCTYPE html>
+        <html lang="es">
+        <head>
+            <meta charset="utf-8">
+            <meta name="robots" content="noindex, nofollow">
+            <title>ARCHIVOS CONTRATOS</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+            <link href="vendor/datetimepicker/bootstrap-datetimepicker.min.css" rel="stylesheet" media="screen">
+            <link href="css/form-invede.css" rel="stylesheet">
+            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+        </head>
+
+        <body>
+        <div class="container contact-form">
+            <div class="contact-image">
+                <img src="img/rocket_contact.png" alt="rocket_contact"/>
+            </div>
+            <h3>ARCHIVOS CONTRATOS</h3>
+                <div class="row">
+
+                    <div class="form-group">
+                        <label for="cedula">CEDULA*</label>
+                         <?php
+$resultado = obtenerListadoDeArchivos("/var/www/html/documentos/archivos", false);
 
 print_r($resultado);
 
@@ -37,5 +70,26 @@ function obtenerListadoDeArchivos($directorio, $recursivo=false){
   }
   $dir->close();
   return $res;
+}
+                         ?>
+                        <div class="mensajecedula"></div>
+                    </div>
+                    
+        </div>
+
+        <script src="vendor/jquery/jquery-1.8.3.min.js"></script>
+        <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="vendor/datetimepicker/bootstrap-datetimepicker.js" charset="UTF-8"></script>
+        <script type="text/javascript" src="vendor/datetimepicker/locales/bootstrap-datetimepicker.es.js"
+                charset="UTF-8"></script>
+        <script type="text/javascript">
+            $(document).ready(function () {
+
+        
+            });
+        </script>
+        </body>
+        </html>
+    <?php }
 }
 ?>
