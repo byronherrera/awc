035512d08a7b6d525f08a61a8558dc689b047e35
byron <byronherrera@hotmail.com>
2020-11-18 00:22:46 -0500

    nuevo reporte

--- aplicaciones/propiedad.php
+++ aplicaciones/propiedad.php
@@ -1,17 +1,62 @@
-<?php
-/**
- * Created AMC developer TEAM.
- * User: bherrera
- * Date: 11/11/2020
- * Time: 9:39
- * Description: link para verificar qr no mover
- */
-?>
-PROPIEDAD: GAD DMQ AGENCIA METROPOLITANA DE CONTROL<br>
-<br>
-AÑO DE ADQUISICIÓN:<?php echo $_GET['a']; ?><br>
-FECHA DE ENTREGA:<?php echo $_GET['f']; ?><br>
-PRENDA: <?php echo $_GET['p']; ?><br>
-CÓDIGO: <?php echo $_GET['c']; ?><br>
-EN CASO DE ENCONTRAR ESTA PRENDA LLAMAR AL TELÉFONO: 3952300 EXT. 28213<br>
-CORREO ELECTRÓNICO: maria.idrobo@quito.gob.ec<br>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Jekyll v4.1.1">
+    <title>Floating labels example · Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="propiedad/assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            -ms-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="propiedad/floating-labels.css" rel="stylesheet">
+</head>
+<body>
+<div class="form-signin">
+    <div class="row">
+        <div class="col-6 themed-grid-col margen0"><img class="mb-4" src="propiedad/assets/logoagencia.png" alt="" width="100%"></div>
+        <div class="col-6 themed-grid-col margen1">
+            <h1 class="h3 mb-3 font-weight-normal"><p>CONTROL DE PRENDAS</p></h1>
+            <p>AÑO DE ADQUISICIÓN 2019</p>
+        </div>
+    </div>
+    <div class="row text-center margen2">
+        <div class="col-8 themed-grid-col">
+            <p>Fecha de entrega: 2020-01-01</p>
+            <p>Prenda: Chompa Plumón</p>
+            <p>Código: 11000-200-02-001</p>
+        </div>
+        <div class="col-4 themed-grid-col">
+            <h1 class="h3 mb-3 font-weight-normal">001</h1>
+        </div>
+    </div>
+
+    <div class="row  text-center margen3 ">
+        <div class="col-12 themed-grid-col">
+            <p class="mt-1 mb-1 text-muted ">EN CASO DE ENCONTRAR ESTA PRENDA</p>
+            <p class="mt-1 mb-1 text-muted ">LLAMAR AL TELEFONO: 3952300 EXT. 28215</p>
+            <p class="mt-1 mb-1 text-muted ">maria.idrovo@quito.gob.ec</p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
